b1dca78460a5074a10080f37e328ac6518f5be6e
Aditya M Joshi <87350102+coderprogrammer1737@users.noreply.github.com>
2021-09-13 09:44:59 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+# include<iostream.h>
+# include<conio.h>
+void main()
+{
+    clrscr();
+    int a,b;
+    
+    cout<<"Enter 2 numbers to know, the greater of the two."<<endl;
+    cin<<a<<b;
+
+    if(a>b);
+    cout<<a<<"is the greater number.";
+    else if(b>a);
+    cout<<b<<"is the greater number.";
+    else
+    cout<<"Both the numbers are equal.";
+    _
+    getch();
+}
